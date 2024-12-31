a8beac3446df36db6ac0a9794d3da06da2ccc08d
predrag jandric <predrag.jandric.bg@gmail.com>
2024-12-31 19:09:01 +0100

    will ADD: gameboard and most of the logic related to it. yet to make Card.jsx component fully

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import Header from "./components/Header";
-// import GameBoard from "./components/GameBoard";
+import GameBoard from "./components/GameBoard";
 
 const App = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <Header />
-      {/* <GameBoard /> */}
+      <GameBoard />
     </div>
   );
 };
---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+// import Card from "./Card";
+
+const GameBoard = () => {
+  const initialCards = ["🍎", "🍌", "🍓", "🍇", "🍎", "🍌", "🍓", "🍇"];
+  const [shuffledCards, setShuffledCards] = useState([]);
+  const [selectedCards, setSelectedCards] = useState([]);
+
+  useEffect(() => {
+    setShuffledCards(shuffleArray(initialCards));
+  }, []);
+
+  const shuffleArray = (array) => {
+    return array
+      .map((value) => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value);
+  };
+
+  const handleCardClick = (index) => {
+    if (selectedCards.length < 2) {
+      setSelectedCards([...selectedCards, index]);
+    }
+  };
+
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      {shuffledCards.map((card, index) => (
+        <p>card</p>
+        
+        // <Card 
+        //   key={index} 
+        //   card={card} 
+        //   index={index} 
+        //   isFlipped={selectedCards.includes(index)} 
+        //   onClick={handleCardClick} 
+        // />
+      ))}
+    </div>
+  );
+};
+
+export default GameBoard;
